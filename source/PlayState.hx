49edd34c2ec9485693b2bde3472c732270c794b0
Myers <skm82s@login.missouristate.edu>
2023-02-16 18:02:21 -0600

    Created Hero class

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,17 @@
 package;
 
+import Hero;
 import flixel.FlxState;
+import haxe.display.Protocol.HaxeResponseErrorData;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+
+		var myHero = new Hero(300, 300);
+		add(myHero);
 	}
 
 	override public function update(elapsed:Float)
