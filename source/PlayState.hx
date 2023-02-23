90ede0b475b1ee2ff90127e031751cfa6ba092c1
Sawyer <greekgeek6467@gmail.com>
2023-02-23 15:24:31 -0600

    Added hello world text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
 import Hero;
 import flixel.FlxState;
 import haxe.display.Protocol.HaxeResponseErrorData;
@@ -11,7 +13,13 @@ class PlayState extends FlxState
 		super.create();
 
 		var myHero = new Hero(300, 300);
+		var helloWorld = new FlxText(240, 150, 0, "Hello world!", 15);
+		var textColor = new FlxColor(0xaa3d7b);
+
+		helloWorld.setFormat("", 30, textColor);
+
 		add(myHero);
+		add(helloWorld);
 	}
 
 	override public function update(elapsed:Float)
