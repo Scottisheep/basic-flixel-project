dd27f8a390aa8b183f19bbb9f9710f3038404045
Sawyer <greekgeek6467@gmail.com>
2023-02-26 16:13:23 -0600

    Finished Controllable Hero and added to playstate

--- source/ControllableHero.hx
+++ source/ControllableHero.hx
@@ -1,18 +1,80 @@
 package;
 
+import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
+import flixel.FlxG;
 
 class ControllableHero extends FlxSprite
 {
+	var directionalAccel = new Array();
+
 	override public function new(x, y)
 	{
 		super(x, y);
-		makeGraphic(35, 35, FlxColor.GREEN);
+		maxVelocity = FlxPoint.get(200, 200);
+		directionalAccel.resize(4);
+		makeGraphic(15, 15, FlxColor.WHITE);
 	}
 
-	override public function update(elapsed:Float)
+	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.anyPressed([UP, W]))
+		{
+			directionalAccel[0] = 150;
+		}
+		else
+		{
+			directionalAccel[0] = 0;
+		}
+
+		if (FlxG.keys.anyPressed([LEFT, A]))
+		{
+			directionalAccel[1] = 150;
+		}
+		else
+		{
+			directionalAccel[1] = 0;
+		}
+
+		if (FlxG.keys.anyPressed([DOWN, S]))
+		{
+			directionalAccel[2] = 150;
+		}
+		else
+		{
+			directionalAccel[2] = 0;
+		}
+
+		if (FlxG.keys.anyPressed([RIGHT, D]))
+		{
+			directionalAccel[3] = 150;
+		}
+		else
+		{
+			directionalAccel[3] = 0;
+		}
+
+		acceleration.y = (directionalAccel[2] - directionalAccel[0]);
+		acceleration.x = (directionalAccel[3] - directionalAccel[1]);
+
+		if (velocity.x > 0)
+		{
+			drag.x = 1.5 * velocity.x;
+		}
+		else
+		{
+			drag.x = -1.5 * velocity.x;
+		}
+		if (velocity.y > 0)
+		{
+			drag.y = 1.5 * velocity.y;
+		}
+		else
+		{
+			drag.y = -1.5 * velocity.y;
+		}
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,10 +1,10 @@
 package;
 
-import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 import flixel.FlxState;
 import ScreensaverHero;
+import ControllableHero;
 
 class PlayState extends FlxState
 {
@@ -12,12 +12,14 @@ class PlayState extends FlxState
 	{
 		super.create();
 
+		var movableHero = new ControllableHero(320, 240);
+
 		var helloWorld = new FlxText(240, 150, 0, "Hello world!", 15);
 		var textColor = new FlxColor(0xaa3d7b);
 
 		var myScreensavers = new Array();
 
-		var numScreensavers:Int = 10;
+		var numScreensavers:Int = 20;
 		for (i in 0...numScreensavers)
 		{
 			myScreensavers.push(new ScreensaverHero(320, 240));
@@ -27,6 +29,7 @@ class PlayState extends FlxState
 		helloWorld.setFormat("", 30, textColor);
 
 		add(helloWorld);
+		add(movableHero);
 	}
 
 	override public function update(elapsed:Float)
